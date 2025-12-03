bd777d068c366ed89399ec3cd183f44f49ee7c34
mbkkt <mbkkt@users.noreply.github.com>
2025-12-03 03:07:47 +0100

    feat: Add Window Functions
    Co-authored-by: pashandor789 <pavel@serenedb.com>

--- axiom/optimizer/RelationOpPrinter.cpp
+++ axiom/optimizer/RelationOpPrinter.cpp
@@ -161,6 +161,11 @@ class ToTextVisitor : public RelationOpVisitor {
     visitDefault(op, context);
   }
 
+  void visit(const WindowOp& op, RelationOpVisitorContext& context)
+      const override {
+    visitDefault(op, context);
+  }
+
   void visit(const UnionAll& op, RelationOpVisitorContext& context)
       const override {
     auto& myCtx = static_cast<Context&>(context);
@@ -311,6 +316,11 @@ class OnelineVisitor : public RelationOpVisitor {
   }
 
   void visit(const OrderBy& op, RelationOpVisitorContext& context)
+      const override {
+    visitDefault(op, context);
+  }
+
+  void visit(const WindowOp& op, RelationOpVisitorContext& context)
       const override {
     visitDefault(op, context);
   }
