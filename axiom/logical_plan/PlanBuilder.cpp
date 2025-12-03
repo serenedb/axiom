72a58a0e37ceebc28f28b78e262fee39a3f58fef
mbkkt <mbkkt@users.noreply.github.com>
2025-12-03 03:07:47 +0100

    feat: Add Window Functions
    Co-authored-by: pashandor789 <pavel@serenedb.com>

--- axiom/logical_plan/PlanBuilder.cpp
+++ axiom/logical_plan/PlanBuilder.cpp
@@ -22,6 +22,7 @@
 #include "velox/duckdb/conversion/DuckParser.h"
 #include "velox/exec/Aggregate.h"
 #include "velox/exec/AggregateFunctionRegistry.h"
+#include "velox/exec/WindowFunction.h"
 #include "velox/expression/Expr.h"
 #include "velox/expression/ExprConstants.h"
 #include "velox/expression/FunctionSignature.h"
@@ -461,6 +462,7 @@ PlanBuilder& PlanBuilder::aggregate(
     const std::vector<ExprApi>& aggregates,
     const std::vector<AggregateOptions>& options) {
   VELOX_USER_CHECK_NOT_NULL(node_, "Aggregate node cannot be a leaf node");
+  VELOX_USER_CHECK_EQ(options.size(), aggregates.size());
 
   std::vector<std::string> outputNames;
   outputNames.reserve(groupingKeys.size() + aggregates.size());
@@ -517,6 +519,165 @@ PlanBuilder& PlanBuilder::aggregate(
       std::move(outputNames));
 
   newOutputMapping->enableUnqualifiedAccess();
+  outputMapping_ = std::move(newOutputMapping);
+
+  return *this;
+}
+
+PlanBuilder::WindowOptions PlanBuilder::parseWindowOptions(
+    const std::string& sql) {
+  auto windowExpr = velox::duckdb::parseWindowExpr(sql, {});
+
+  WindowExpr::Frame frame;
+  // Convert window type
+  switch (windowExpr.frame.type) {
+    case velox::duckdb::WindowType::kRows:
+      frame.type = WindowExpr::WindowType::kRows;
+      break;
+    case velox::duckdb::WindowType::kRange:
+      frame.type = WindowExpr::WindowType::kRange;
+      break;
+  }
+
+  switch (windowExpr.frame.startType) {
+    case velox::duckdb::BoundType::kUnboundedPreceding:
+      frame.startType = WindowExpr::BoundType::kUnboundedPreceding;
+      break;
+    case velox::duckdb::BoundType::kPreceding:
+      frame.startType = WindowExpr::BoundType::kPreceding;
+      break;
+    case velox::duckdb::BoundType::kCurrentRow:
+      frame.startType = WindowExpr::BoundType::kCurrentRow;
+      break;
+    case velox::duckdb::BoundType::kFollowing:
+      frame.startType = WindowExpr::BoundType::kFollowing;
+      break;
+    case velox::duckdb::BoundType::kUnboundedFollowing:
+      frame.startType = WindowExpr::BoundType::kUnboundedFollowing;
+      break;
+  }
+
+  switch (windowExpr.frame.endType) {
+    case velox::duckdb::BoundType::kUnboundedPreceding:
+      frame.endType = WindowExpr::BoundType::kUnboundedPreceding;
+      break;
+    case velox::duckdb::BoundType::kPreceding:
+      frame.endType = WindowExpr::BoundType::kPreceding;
+      break;
+    case velox::duckdb::BoundType::kCurrentRow:
+      frame.endType = WindowExpr::BoundType::kCurrentRow;
+      break;
+    case velox::duckdb::BoundType::kFollowing:
+      frame.endType = WindowExpr::BoundType::kFollowing;
+      break;
+    case velox::duckdb::BoundType::kUnboundedFollowing:
+      frame.endType = WindowExpr::BoundType::kUnboundedFollowing;
+      break;
+  }
+
+  if (windowExpr.frame.startValue != nullptr) {
+    frame.startValue = resolveScalarTypes(windowExpr.frame.startValue);
+  }
+  if (windowExpr.frame.endValue != nullptr) {
+    frame.endValue = resolveScalarTypes(windowExpr.frame.endValue);
+  }
+
+  std::vector<ExprPtr> partitionBy;
+  partitionBy.reserve(windowExpr.partitionBy.size());
+  for (const auto& partitionExpr : windowExpr.partitionBy) {
+    partitionBy.emplace_back(resolveScalarTypes(partitionExpr));
+  }
+
+  std::vector<SortingField> orderBy;
+  orderBy.reserve(windowExpr.orderBy.size());
+  for (const auto& orderByClause : windowExpr.orderBy) {
+    auto sortKeyExpr = resolveScalarTypes(orderByClause.expr);
+    SortOrder order{orderByClause.ascending, orderByClause.nullsFirst};
+    orderBy.emplace_back(sortKeyExpr, order);
+  }
+
+  return {
+      std::move(partitionBy),
+      std::move(orderBy),
+      std::move(frame),
+      windowExpr.ignoreNulls};
+}
+
+PlanBuilder& PlanBuilder::window(const std::vector<std::string>& windowExprs) {
+  std::vector<ExprApi> parsedExprs;
+  std::vector<WindowOptions> options;
+  parsedExprs.reserve(windowExprs.size());
+  options.reserve(windowExprs.size());
+
+  for (const auto& sql : windowExprs) {
+    auto parsed = velox::duckdb::parseWindowExpr(sql, {});
+
+    parsedExprs.emplace_back(parsed.functionCall);
+    options.emplace_back(parseWindowOptions(sql));
+  }
+
+  return window(parsedExprs, options);
+}
+
+PlanBuilder& PlanBuilder::window(
+    const std::vector<ExprApi>& windowExprs,
+    const std::vector<WindowOptions>& options) {
+  VELOX_USER_CHECK_NOT_NULL(node_, "Window node cannot be a leaf node");
+  VELOX_USER_CHECK(options.size() == windowExprs.size());
+
+  std::vector<std::string> outputNames;
+  outputNames.reserve(windowExprs.size());
+  std::vector<WindowExprPtr> exprs;
+  exprs.reserve(windowExprs.size());
+  auto newOutputMapping = std::make_shared<NameMappings>(*outputMapping_);
+
+  for (size_t i = 0; i < windowExprs.size(); ++i) {
+    const auto& windowExpr = windowExprs[i];
+    const auto& windowOptions = options[i];
+
+    auto resolveResult = resolveWindowTypes(windowExpr.expr());
+
+    WindowExprPtr expr = std::make_shared<WindowExpr>(
+        resolveResult.type,
+        resolveResult.functionName,
+        resolveResult.functionInputs,
+        windowOptions.partitionBy,
+        windowOptions.orderBy,
+        windowOptions.frame,
+        windowOptions.ignoreNulls);
+
+    if (windowExpr.name().has_value()) {
+      const auto& alias = windowExpr.name().value();
+      outputNames.push_back(newName(alias));
+      newOutputMapping->add(alias, outputNames.back());
+    } else {
+      outputNames.push_back(newName(expr->name()));
+    }
+
+    exprs.emplace_back(std::move(expr));
+  }
+
+  const auto& inputType = node_->outputType();
+  std::vector<std::string> allNames;
+  allNames.reserve(inputType->size() + exprs.size());
+  std::vector<ExprPtr> allExprs;
+  allExprs.reserve(inputType->size() + exprs.size());
+
+  for (size_t i = 0; i < inputType->size(); ++i) {
+    allNames.push_back(inputType->nameOf(i));
+    allExprs.push_back(
+        std::make_shared<InputReferenceExpr>(
+            inputType->childAt(i), inputType->nameOf(i)));
+  }
+
+  for (size_t i = 0; i < exprs.size(); ++i) {
+    allNames.push_back(outputNames[i]);
+    allExprs.push_back(exprs[i]);
+  }
+
+  node_ = std::make_shared<ProjectNode>(
+      nextId(), std::move(node_), std::move(allNames), std::move(allExprs));
+
   outputMapping_ = std::move(newOutputMapping);
 
   return *this;
@@ -1267,6 +1428,72 @@ AggregateExprPtr ExprResolver::resolveAggregateTypes(
   }
 }
 
+ExprResolver::WindowResolveResult ExprResolver::resolveWindowTypes(
+    const velox::core::ExprPtr& expr,
+    const InputNameResolver& inputNameResolver) const {
+  const auto* call = dynamic_cast<const velox::core::CallExpr*>(expr.get());
+  VELOX_USER_CHECK_NOT_NULL(
+      call, "Window function must be a call expression: {}", expr->toString());
+
+  const auto& name = call->name();
+
+  std::vector<ExprPtr> inputs;
+  inputs.reserve(expr->inputs().size());
+  for (const auto& input : expr->inputs()) {
+    inputs.push_back(resolveScalarTypes(input, inputNameResolver));
+  }
+
+  std::vector<velox::TypePtr> inputTypes;
+  inputTypes.reserve(inputs.size());
+  for (const auto& input : inputs) {
+    inputTypes.push_back(input->type());
+  }
+
+  // First try window function signatures
+  auto windowSignatures = velox::exec::getWindowFunctionSignatures(name);
+  if (windowSignatures.has_value()) {
+    for (const auto& signature : windowSignatures.value()) {
+      velox::exec::SignatureBinder binder(*signature, inputTypes);
+      if (binder.tryBind()) {
+        if (auto type = binder.tryResolveReturnType()) {
+          return {type, name, inputs};
+        }
+      }
+    }
+  }
+
+  auto aggregateSignatures = velox::exec::getAggregateFunctionSignatures(name);
+  if (aggregateSignatures.has_value()) {
+    for (const auto& signature : aggregateSignatures.value()) {
+      velox::exec::SignatureBinder binder(*signature, inputTypes);
+      if (binder.tryBind()) {
+        if (auto type = binder.tryResolveReturnType()) {
+          return {type, name, inputs};
+        }
+      }
+    }
+  }
+
+  if (!windowSignatures.has_value() && !aggregateSignatures.has_value()) {
+    VELOX_USER_FAIL("Window Function doesn't exist: {}.", name);
+  } else {
+    std::string errorMsg = "Window function signature is not supported: " +
+        toString(name, inputTypes) + ". ";
+    if (windowSignatures.has_value()) {
+      errorMsg +=
+          "Window function signatures: " + toString(windowSignatures.value()) +
+          ". ";
+    }
+    if (aggregateSignatures.has_value()) {
+      errorMsg += "Aggregate function signatures: " +
+          toString(aggregateSignatures.value()) + ".";
+    }
+    VELOX_USER_FAIL(errorMsg);
+  }
+
+  VELOX_UNREACHABLE();
+}
+
 PlanBuilder& PlanBuilder::join(
     const PlanBuilder& right,
     const std::string& condition,
@@ -1362,6 +1589,38 @@ PlanBuilder& PlanBuilder::setOperation(
     nodes.push_back(builder.node_);
   }
   node_ = std::make_shared<SetNode>(nextId(), std::move(nodes), op);
+  return *this;
+}
+
+PlanBuilder& PlanBuilder::orderByWindows(
+    const std::vector<std::string>& sortingKeys) {
+  VELOX_USER_CHECK_NOT_NULL(node_, "Sort node cannot be a leaf node");
+
+  std::vector<SortingField> sortingFields;
+  sortingFields.reserve(sortingKeys.size());
+
+  for (const auto& key : sortingKeys) {
+    auto parsedWindowExpr = velox::duckdb::parseWindowExpr(key, {});
+    auto windowOptions = parseWindowOptions(key);
+    auto resolveResult = resolveWindowTypes(parsedWindowExpr.functionCall);
+
+    WindowExprPtr windowExpr = std::make_shared<WindowExpr>(
+        resolveResult.type,
+        resolveResult.functionName,
+        resolveResult.functionInputs,
+        windowOptions.partitionBy,
+        windowOptions.orderBy,
+        windowOptions.frame,
+        windowOptions.ignoreNulls);
+
+    sortingFields.push_back(
+        SortingField{
+            windowExpr, SortOrder(true, false)}); // ascending, nulls last
+  }
+
+  node_ = std::make_shared<SortNode>(
+      nextId(), std::move(node_), std::move(sortingFields));
+
   return *this;
 }
 
@@ -1563,6 +1822,14 @@ AggregateExprPtr PlanBuilder::resolveAggregateTypes(
       filter,
       ordering,
       distinct);
+}
+
+PlanBuilder::WindowResolveResult PlanBuilder::resolveWindowTypes(
+    const velox::core::ExprPtr& expr) const {
+  return resolver_.resolveWindowTypes(
+      expr, [&](const auto& alias, const auto& name) {
+        return resolveInputName(alias, name);
+      });
 }
 
 PlanBuilder& PlanBuilder::as(const std::string& alias) {
